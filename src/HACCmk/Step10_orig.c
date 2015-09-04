95aeda33343d707c5d230d7a74319fe25a11b6ee
sidkashyap <sidharth.n.kashyap@gmail.com>
2015-09-04 18:11:45 +0100

    plots

--- src/HACCmk/Step10_orig.c
+++ src/HACCmk/Step10_orig.c
@@ -6,11 +6,17 @@
 
 #include <math.h>
 
+//sid-> this is the function executed  by all the threads
+//xx,yy,zz - particles
+
+
 void Step10_orig( int count1, float xxi, float yyi, float zzi, float fsrrmax2, float mp_rsm2, float *xx1, float *yy1, float *zz1, float *mass1, float *dxi, float *dyi, float *dzi )
 {
 
+	//sid-> some magic numbers
     const float ma0 = 0.269327, ma1 = -0.0750978, ma2 = 0.0114808, ma3 = -0.00109313, ma4 = 0.0000605491, ma5 = -0.00000147177;
     
+    //sid-> some floats
     float dxc, dyc, dzc, m, r2, f, xi, yi, zi;
     int j;
 
@@ -26,15 +32,17 @@ void Step10_orig( int count1, float xxi, float yyi, float zzi, float fsrrmax2, f
        
         m = ( r2 < fsrrmax2 ) ? mass1[j] : 0.0f;
 
+        //The force is approximated with a power function over the distance between particles and a 5-th order polynomial in the reminder
         f =  pow( r2 + mp_rsm2, -1.5 ) - ( ma0 + r2*(ma1 + r2*(ma2 + r2*(ma3 + r2*(ma4 + r2*ma5)))));
-        
         f = ( r2 > 0.0f ) ? m * f : 0.0f;
 
+        //sid->when doing integration, always multiply the stepsize
         xi = xi + f * dxc;
         yi = yi + f * dyc;
         zi = zi + f * dzc;
     }
 
+    //sid-> write the results back
     *dxi = xi;
     *dyi = yi;
     *dzi = zi;
--- src/HACCmk/main.c
+++ src/HACCmk/main.c
@@ -47,58 +47,87 @@ extern double mysecond();
 
 int main( int argc, char *argv[] )
 {
+
+  //sid-> float arrays
   static float xx[N], yy[N], zz[N], mass[N], vx1[N], vy1[N], vz1[N];
+  //sid-> some more floats
   float fsrrmax2, mp_rsm2, fcoeff, dx1, dy1, dz1;
 
+  //sid-> char arrays
   static char  M1[NC], M2[NC];
+  //sid-> some more ints
   int n, count, i, rank, nprocs;
+
+  //sid-> big ints
   unsigned long long tm1, tm2, tm3, tm4, total = 0;
+
+  //sid-> a few doubles
   double t1,t2,t3, elapsed = 0.0, validation, final;
+
+  //sid-> No MPI for ya!
 
   //MPI_Init( &argc, &argv );
   //MPI_Comm_rank( MPI_COMM_WORLD, &rank );
   //MPI_Comm_size( MPI_COMM_WORLD, &nprocs );
   
+  //sid-> but we use the same rank analogy
   rank = 0;
+  //sid-> yes sir, this is openmp version
   nprocs = 1;
 
+
+  //sid-> TODO, mystery number 327, what does this mean?
   count = 327;
 
+  //sid-> executed by the main thread (the only thread rather)
   if ( rank == 0 ) 
   {  
      printf( "count is set %d\n", count );
      printf( "Total MPI ranks %d\n", nprocs );
   } 
 
+  //sid-> the parallel game begins
+
 #pragma omp parallel
 {
+	//sid-> print number of threads, to be done by the master thread
   if ( (rank == 0) && (omp_get_thread_num() == 0) )
   {
+
      printf( "Number of OMP threads %d\n\n", omp_get_num_threads() );
      //printf( "      N         Time,us        Validation result\n" );
   }   
 }
+
 
 #ifdef TIMEBASE
   tm3 = timebase();
 #endif
 
   final = 0.;
-
+  //sid-> this looks to be the main loop, TODO, why do we jump 20 and start at 400 boss?
   for ( n = 400; n < N; n = n + 20 ) 
   {
       /* Initial data preparation */
-      fcoeff = 0.23f;  
+	  //sid-> some physics numbers that I dont care about
+	  fcoeff = 0.23f;
       fsrrmax2 = 0.5f; 
       mp_rsm2 = 0.03f;
+
+
+      //sid-> this is how we do differentiation on the computer, dx/dt - basically divide it as small as possible and add
       dx1 = 1.0f/(float)n;
       dy1 = 2.0f/(float)n;
       dz1 = 3.0f/(float)n;
+
+      //sid-> init the array
       xx[0] = 0.f;
       yy[0] = 0.f;
       zz[0] = 0.f;
       mass[0] = 2.f;
       
+      //sid-> init the array - from i=1 to n, sigma(x[i-1]+dx1)
+      //sid-> these represent particles
       for ( i = 1; i < n; i++ )
       {
           xx[i] = xx[i-1] + dx1;
@@ -106,6 +135,8 @@ int main( int argc, char *argv[] )
           zz[i] = zz[i-1] + dz1;
           mass[i] = (float)i * 0.01f + xx[i];
       }
+
+      //sid-> vx is init to zero
     
       for ( i = 0; i < n; i++ )
       {
@@ -116,10 +147,19 @@ int main( int argc, char *argv[] )
     
       /* Data preparation done */
     
+
+      //sid-> this is very interesting, clean the L1 cache, flush it with some number, in this case 4. but the point is, will it really do it? TODO
+      //sid-> this is the declaration, FYI static char  M1[NC], M2[NC];
+      //sid-> it is very interesting to note the importance of static vars in openmp
+      //sid-> for this to really clean the cache, NC has to really big, and NC is init to be 16777216
+      //sid-> that translates to 16MB or MiB to keep google happy
+
     
       /* Clean L1 cache */
       for ( i = 0; i < NC; i++ ) M1[i] = 4;
       for ( i = 0; i < NC; i++ ) M2[i] = M1[i];
+
+      //sid-> init some more time variables
 
 #ifdef TIMEBASE
       tm1 = timebase();
@@ -127,9 +167,28 @@ int main( int argc, char *argv[] )
       t1 = mysecond();
 #endif
 
+      //sid-> this is a nesting, to be executed by each thread
+      /*
+       * regular stride one memory accesses
+       *  It is looping over a predefined list of particles and their neighborhoods and calculates the force values for each particle.
+       *
+       *
+       * The force evaluation for each particle can be independently evaluated
+       * the current implementation is using OpenMP “parallel for” pragma statement to parallelize over the loop of particles.
+       *
+       * The calculation of the force for each particle is implemented as a separate function with particle coordinates, their mass, the
+       * list of coordinates of neighborhood particles, and their masses, as the input arguments.
+       * */
+
+
+    /*
+     * dx1, dx2 and dz1 are to be private as the values are changed on each iteration
+     * */
+
     #pragma omp parallel for private( dx1, dy1, dz1 )
       for ( i = 0; i < count; ++i)
       {
+    	  //sid-> this is the function executed by each thread multiple times on each iteration
         Step10_orig( n, xx[i], yy[i], zz[i], fsrrmax2, mp_rsm2, xx, yy, zz, mass, &dx1, &dy1, &dz1 );
     
         vx1[i] = vx1[i] + dx1 * fcoeff;
@@ -137,11 +196,14 @@ int main( int argc, char *argv[] )
         vz1[i] = vz1[i] + dz1 * fcoeff;
       }
 
+      //sid-> some more timing numbers
 #ifdef TIMEBASE
       tm2 = timebase();
 #else
       t2 = mysecond();
 #endif
+
+      //sid-> the threads have finished computing - now validate the results
 
       validation = 0.;
       for ( i = 0; i < n; i++ )
@@ -150,6 +212,8 @@ int main( int argc, char *argv[] )
       }
 
       final = final + validation;
+
+      //sid-> what is the time taken in total
     
 #ifdef TIMEBASE      
       t3 = 1e6 * (double)(tm2 - tm1) / MHz; // time in us
@@ -160,7 +224,7 @@ int main( int argc, char *argv[] )
       elapsed = elapsed + t3;
     
   }
-
+  //sid-> Print the timing results obtained
 #ifdef TIMEBASE
   tm4 = timebase();
   if ( rank == 0 )

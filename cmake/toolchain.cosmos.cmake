861db79ed143915a9a236f9e756993cb477b62f5
Richard Rollins <rprollins@users.noreply.github.com>
2016-07-07 18:37:54 +0100

    COSMOS CMake toolchain and Moab jobscript template

---
+++ cmake/toolchain.cosmos.cmake
@@ -0,0 +1,17 @@
+# Compilers
+set(CMAKE_C_COMPILER       /opt/sgi/mpt/mpt-2.13/bin/mpicc)
+set(CMAKE_CXX_COMPILER     /opt/sgi/mpt/mpt-2.13/bin/mpicxx)
+set(CMAKE_Fortran_COMPILER /opt/sgi/mpt/mpt-2.13/bin/mpif90)
+
+# Hardware Specification
+set(DIRAC3_HOST          cosmos)
+set(HOST_CPUS_PER_NODE        8)
+set(HOST_THREADS_PER_CPU      1)
+
+# OpenMP flags (CMake cannot detect itself)
+set(OPENMP_FOUND True)
+set(OpenMP_C_FLAGS "-fopenmp=libomp")
+set(OpenMP_CXX_FLAGS "-fopenmp=libomp")
+set(OpenMP_Fortran_FLAGS "-fopenmp")
+
+set(FFTW2_ROOT $ENV{FFTW2_ROOT})
